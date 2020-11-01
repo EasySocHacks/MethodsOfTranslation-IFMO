e60eb3707e86b00fd8f3e58bb54de27a1c72c0ee
EasySocHcks <easy.soc.hacks@gmail.com>
2020-11-01 23:20:54 +0300

    Add Main.java for testing

--- Lab1/src/test/java/Main.java
+++ Lab1/src/test/java/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import grammar.Grammar;
+import grammar.GrammarParser;
+import yaml.YamlReader;
 
 public class Main {
+    public static void main(String[] args) {
+        GrammarParser grammarParser = new GrammarParser();
+        Grammar grammar = grammarParser.getGrammar();
+    }
 }
